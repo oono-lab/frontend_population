d667bed6e9bec7195787215f18b18e0e39b795bb
oono-lab <145289101+oono-lab@users.noreply.github.com>
2024-04-10 09:32:24 +0900

    「都道府県一覧」のデータを取得する

---
+++ src/api_information/usePrefectures.tsx
@@ -0,0 +1,40 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+import { apiKey } from './api_code_key';
+const usePrefectures = () => {
+  const [prefectures, setPrefectures] = useState<
+    { code: string; name: string }[]
+  >([])
+
+  useEffect(() => {
+    const fetchPrefectures = async () => {
+      try {
+        const response = await axios.get(
+          'https://opendata.resas-portal.go.jp/api/v1/prefectures',
+          {
+            headers: {
+              'X-API-KEY': apiKey,
+            },
+          }
+        )
+        //
+        const data = response.data.result
+
+        setPrefectures(
+          data.map((prefecture: any) => ({
+            code: prefecture.prefCode,
+            name: prefecture.prefName,
+          }))
+        )
+      } catch (error) {
+        console.error('Error fetching prefectures:', error)
+      }
+    }
+
+    fetchPrefectures()
+  }, [])
+
+  return prefectures
+}
+
+export default usePrefectures
